f6b19a212e959d9bda126b8a971fcffa75d8fb59
Pulixe <francisco.pulice@outlook.com>
2023-09-12 14:30:49 -0500

    add css

---
+++ README.md
@@ -0,0 +1,8 @@
+# Bootstrap 5 Portfolio Template
+
+### TEMPLATE SCREENSHOT
+
+### WEBSITE PREVIEW 
+
+[BOOTSTRAP 5 TEMPLATE PREVIEW ](https://bootstrap-5-website.netlify.app/)
+
---
+++ assets/js/model-view.js
@@ -0,0 +1,15 @@
+
+/*(() => {
+  const modelViewer = document.querySelector('#modelviewer');
+  const orbitCycle = [
+    '45deg 55deg 4m',
+    '-60deg 110deg 2m',
+    modelViewer.cameraOrbit
+  ];
+
+  setInterval(() => {
+    const currentOrbitIndex = orbitCycle.indexOf(modelViewer.cameraOrbit);
+    modelViewer.cameraOrbit =
+        orbitCycle[(currentOrbitIndex + 1) % orbitCycle.length];
+  }, 3000);
+})();*/
